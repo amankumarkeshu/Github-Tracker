f4b5557b92f05f3a8f9f6a32658f7243534de415
Aman Kumar Kesu <aman.kesu@crio-users.in>
2020-12-03 20:45:13 +0530

    [ Code Refactored]

--- app.js
+++ app.js
@@ -11,25 +11,46 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 var port = process.env.port || 3000;
+  
+app.get("/",  (req, res)=> {
+    res.render("home.ejs");
+});
 
 app.get("/index", function(req, res){
     res.sendFile(path.join(__dirname, 'views/viewPage.html')); 
 
 });
-  
 
-app.get("/",  (req, res)=> {
-    res.render("home.ejs");
-});
-
-let globalUsername='';
-let globalRepo='';
+function commits(username,repo){
+    const commitUrl = 'https://api.github.com/repos/'+username+'/'+repo+'/stats/commit_activity';
+    (async () => {
+        const response = await fetch(commitUrl);
+        const json = await response.json();
+        console.log(json);
+        let commits=[];
+        for( let obj = 0; obj <json.length;obj++){
+            let object= {
+                number:obj+1,
+                total:json[obj].total,
+                week:json[obj].week,
+                days:json[obj].days
+            }
+            commits.push(object);
+        }
+        console.log("yes",commits);
+        convertor.json2csv(commits, (err,csv) =>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                fs.writeFileSync('activity.csv',csv);
+            }
+        });
+    })();
+}
 
 function additions(username,repo){
     const url = 'https://api.github.com/repos/'+username+'/'+repo+'/stats/code_frequency';
@@ -85,160 +106,26 @@ function deletions(username,repo){
     })();
 }
 
-// app.get("/commits",(req,res)=>{
-//     let username = globalUsername;
-//     let repo = globalRepo;
-//     if(username==='' || repo==='') 
-//         res.render("home");
-//     const url = 'https://api.github.com/repos/'+username+'/'+repo+'/stats/commit_activity';
-//     (async () => {
-//         const response = await fetch(url);
-//         const json = await response.json();
-//         console.log(json);
-//         let arr=[];
-//         for( let obj = 0; obj <json.length;obj++){
-//             let object= {
-//                 number:obj+1,
-//                 total:json[obj].total,
-//                 week:json[obj].week,
-//                 days:json[obj].days
-//             }
-//             arr.push(object);
-//         }
-//         console.log("yes",arr);
-//         convertor.json2csv(arr, (err,csv) =>{
-//             if(err){
-//                 console.log(err);
-//             }
-//             else{
-//                 fs.writeFileSync('activity.csv',csv);
-//             }
-//         });
-//         var yourscript = exec('Rscript myscript.r',(error,stdout,stderr) =>{
-//             console.log(stdout);
-//             console.log(stderr);
-//             if(error!==null){
-//                 console.log(`exec error :${error}`);
-//             }
-//         });
-
-//     })();
-//     res.render("viewPage");
-// });
-
-
-// app.get("/addition",(req,res)=>{
-//     let username = globalUsername;
-//     let repo = globalRepo;
-//     if(username==='' || repo==='') 
-//         res.render("home");
-//     const url = 'https://api.github.com/repos/'+username+'/'+repo+'/stats/code_frequency';
-//     (async () => {
-//         const response = await fetch(url);
-//         const json = await response.json();
-//         let arr=[];
-//         for( let obj = 0; obj <json.length;obj++){
-//             let object= {
-//                 number:obj+1,
-//                 week:json[obj][0],
-//                 addition:json[obj][1]
-//             }
-//             arr.push(object);
-//         }
-//         console.log("yes",arr);
-//         convertor.json2csv(arr, (err,csv) =>{
-//             if(err){
-//                 console.log(err);
-//             }
-//             else{
-//                 fs.writeFileSync('additions.csv',csv);
-//             }
-//         })
-
-//     })();
-//     res.render("viewPage");
-// });
-
-
-// app.get("/deletion",(req,res)=>{
-//     let username = globalUsername;
-//     let repo = globalRepo;
-//     if(username==='' || repo==='') 
-//         res.render("home");
-//     const url = 'https://api.github.com/repos/'+username+'/'+repo+'/stats/code_frequency';
-//     (async () => {
-//         const response = await fetch(url);
-//         const json = await response.json();
-//         let arr=[];
-//         for( let obj = 0; obj <json.length;obj++){
-//             let object= {
-//                 number:obj+1,
-//                 week:json[obj][0],
-//                 deletion:json[obj][2]
-//             }
-//             arr.push(object);
-//         }
-
-//         convertor.json2csv(arr, (err,csv) =>{
-//             if(err){
-//                 console.log(err);
-//             }
-//             else{
-//                 fs.writeFileSync('deletions.csv',csv);
-//             }
-//         })
-
-//     })();
-//     res.render("viewPage");
-// });
-
-
-
 app.post("/",  (req, res)=> {
     let username = req.body.username;
     let repo = req.body.repo;
     if(username==='' || repo==='') 
         res.render("home.ejs");
-    const commitUrl = 'https://api.github.com/repos/'+username+'/'+repo+'/stats/commit_activity';
-    (async () => {
-        const response = await fetch(commitUrl);
-        const json = await response.json();
-        console.log(json);
-        let commits=[];
-        for( let obj = 0; obj <json.length;obj++){
-            let object= {
-                number:obj+1,
-                total:json[obj].total,
-                week:json[obj].week,
-                days:json[obj].days
-            }
-            commits.push(object);
+
+    commits(username,repo);
+    additions(username,repo);
+    deletions(username,repo);
+
+    var yourscript = exec('Rscript myscript.r',(error,stdout,stderr) =>{
+        console.log(stdout);
+        console.log(stderr);
+        if(error!==null){
+            console.log(`exec error :${error}`);
         }
-        console.log("yes",commits);
-        convertor.json2csv(commits, (err,csv) =>{
-            if(err){
-                console.log(err);
-            }
-            else{
-                fs.writeFileSync('activity.csv',csv);
-            }
-        });
-        additions(username,repo);
-        deletions(username,repo);
-
-        var yourscript = exec('Rscript myscript.r',(error,stdout,stderr) =>{
-            console.log(stdout);
-            console.log(stderr);
-            if(error!==null){
-                console.log(`exec error :${error}`);
-            }
-        });
+    });
     
-    })();
     res.sendFile(path.join(__dirname, 'views/viewPage.html')); 
-    // res.render("viewPage");
 });
-
 
 
 app.listen(port, ()=> {
--- myscript.r
+++ myscript.r
@@ -1,10 +1,13 @@
 #!
 commitData <- read.csv("activity.csv")
-barplot(height=commitData$total,names.arg=commitData$number)
+barplot(height=commitData$total,names.arg=commitData$number,xlab = "Week",
+ylab = "Commits")
 
 additionData <- read.csv("additions.csv")
-barplot(height=additionData$addition,names.arg=additionData$number)
+barplot(height=additionData$addition,names.arg=additionData$number,xlab = "Week",
+ylab = "Additions")
 
 additionData
 deletionData <- read.csv("deletions.csv")
-barplot(height=deletionData$deletion,names.arg=deletionData$number)
+barplot(height=deletionData$deletion,names.arg=deletionData$number,xlab = "Week",
+ylab = "Deletions")
